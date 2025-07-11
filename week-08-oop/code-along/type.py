48608540e993e405ff1a9c3510e3353b25527a30
Manish Bhatt <manishbhatt594@gmail.com>
2025-07-11 19:27:46 +0530

    ch-08-oop: built in data types as classes

---
+++ week-08-oop/code-along/type.py
@@ -0,0 +1,11 @@
+print(type(50))  # prints: "<class 'int'>"
+
+print(type("hello, world"))  # prints: "<class 'str'>"
+
+print(type([]))  # prints: "<class 'list'>"
+
+print(type(list()))  # prints: "<class 'list'>"
+
+print(type({}))  # prints: "<class 'dict'>"
+
+print(type(dict()))  # prints: "<class 'dict'>"
