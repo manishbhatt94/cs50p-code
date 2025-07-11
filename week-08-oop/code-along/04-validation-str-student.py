532e347fc1cdebeb9d63b1d23287824203eea6ab
Manish Bhatt <manishbhatt594@gmail.com>
2025-07-11 16:24:29 +0530

    ch-08-oop - Validate in init. Str method override.

---
+++ week-08-oop/code-along/04-validation-str-student.py
@@ -0,0 +1,36 @@
+"""
+- Validation inside constructor to prevent invalid object instantiation
+
+- Dunder str (__str__) for printing an object - meant for end users viewing
+
+- Dunder repr (__repr__ for 'representation') for another (detailed) string
+representation of an object but which is meant for developers for debugging etc
+"""
+
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+
+def main():
+    student = get_student()
+    print(student)
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+if __name__ == "__main__":
+    main()
