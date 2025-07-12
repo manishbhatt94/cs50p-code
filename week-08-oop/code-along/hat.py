85b1743c5484b8a1b178af17904398d2537abc01
Manish Bhatt <manishbhatt594@gmail.com>
2025-07-12 16:04:31 +0530

    ch-08-oop - Correction in comments about classmethod

--- week-08-oop/code-along/hat.py
+++ week-08-oop/code-along/hat.py
@@ -1,11 +1,14 @@
 import random
 
 
+# Note that: static members are different than class members in Python
+# and have a separate annotation called `@staticmethod` (separate from `@classmethod`)
+# TODO: Research about "static" members in Python, & how they differ from "class" members
 class Hat:
-    # class (static) variable 'houses'
+    # class variable 'houses'
     houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
 
-    # class (static) method 'sort'
+    # class method 'sort'
     @classmethod
     def sort(cls, name):
         # ^^^^^ instead of 'self', 1st param is 'cls' which is reference to the current class
