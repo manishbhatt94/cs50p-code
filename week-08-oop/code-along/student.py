a7dbdc63c07be1e6cabde96ec7cfd599cbcab0bb
Manish Bhatt <manishbhatt594@gmail.com>
2025-07-12 10:05:35 +0530

    ch-08-oop - Move get_student inside Student class as a class method

--- week-08-oop/code-along/student.py
+++ week-08-oop/code-along/student.py
@@ -1,4 +1,7 @@
-""" """
+"""
+Demo of how class method can be used to get an instance of the class
+Factory pattern kind of
+"""
 
 
 class Student:
@@ -9,16 +12,16 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
-
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house)
 
 
 if __name__ == "__main__":
